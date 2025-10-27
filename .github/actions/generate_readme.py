b806ff6dee7573d37f63f89cde28c45fc223ec72
Cody Keats <cocopuff2u@yahoo.com>
2025-10-27 09:11:58 -0400

    Fixing Format

--- .github/actions/generate_readme.py
+++ .github/actions/generate_readme.py
@@ -161,7 +161,7 @@ BROWSER_CONFIGS = {
         'fetch_details': fetch_chrome_details,
         'channels': [
             {'name': '', 'display': 'Chrome', 'version_path': 'stable/version', 'download_path': 'stable/latest_download', 'bundle_id': 'com.google.Chrome', 'image': 'chrome.png', 'release_notes': 'https://chromereleases.googleblog.com/'},
-            {'name': 'Extended Stable', 'display': 'Chrome', 'version_path': 'extended/version', 'download_path': 'extended/latest_download', 'bundle_id': 'com.google.Chrome', 'image': 'chrome.png', 'release_notes_comment': '_Requires `TargetChannel` policy; link is for Stable._'},
+            {'name': 'Extended Stable', 'display': 'Chrome', 'version_path': 'extended/version', 'download_path': 'extended/latest_download', 'bundle_id': 'com.google.Chrome', 'image': 'chrome.png', 'release_notes_comment': '<br>_<sup>Requires `TargetChannel` policy; link is for Stable.</sup>_'},
             {'name': 'Beta', 'display': 'Chrome', 'version_path': 'beta/version', 'download_path': 'beta/beta_download', 'bundle_id': 'com.google.Chrome.beta', 'image': 'chrome_beta.png', 'release_notes': 'https://chromereleases.googleblog.com/search/label/Beta%20updates'},
             {'name': 'Dev', 'display': 'Chrome', 'version_path': 'dev/version', 'download_path': 'dev/dev_download', 'bundle_id': 'com.google.Chrome.dev', 'image': 'chrome_dev.png', 'release_notes': 'https://chromereleases.googleblog.com/search/label/Dev%20updates'},
             {'name': 'Canary', 'display': 'Chrome', 'version_path': 'canary/version', 'download_path': 'canary/canary_download', 'bundle_id': 'com.google.Chrome.canary', 'image': 'chrome_canary.png'}
